f13f9dc94a9be7ebe3493d852f493bc6027f7fe0
raniabraham81 <Abraham Daniel@10.0.0.175>
2020-12-23 21:49:16 -0700

    Testing GitHub

--- src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -9,6 +9,7 @@ public class SfgPetClinicApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SfgPetClinicApplication.class, args);
 		System.out.println("Trying to commit to Git hub");
+		System.out.println("Trying again");
 	}
 
 }
